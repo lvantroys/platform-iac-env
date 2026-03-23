8c9fbaba7ac49be37a9ee64472ed7ef65020b9a6
Luc Van3 <lootchino@hotmail.com>
2026-03-23 12:47:49 -0500

    Formatted file

--- live/dev/10-vpc/variables.tf
+++ live/dev/10-vpc/variables.tf
@@ -3,16 +3,16 @@ variable "environment" { type = string }
 variable "app" { type = string }
 variable "owner" { type = string }
 variable "data_classification" { type = string }
-variable "extra_tags" { 
-    type = map(string)
-    default = {} 
-    }
+variable "extra_tags" {
+  type    = map(string)
+  default = {}
+}
 
 variable "name_prefix" { type = string }
 variable "vpc_cidr" { type = string }
 variable "public_subnet_cidrs" { type = list(string) }
 variable "private_subnet_cidrs" { type = list(string) }
-variable "enable_nat_gateway" { 
-    type = bool
-    default = true 
-    }
+variable "enable_nat_gateway" {
+  type    = bool
+  default = true
+}
